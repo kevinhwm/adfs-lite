e2db2ce4c3328d5ebddddeee79f9458fcfa01503
kevin <huangtao@antiy.com>
2013-07-16 17:32:06 +0800

    init v3.2.2

---
+++ src/bin/main.c
@@ -0,0 +1,277 @@
+
+#include "nxweb/nxweb.h"
+#include <stdio.h>
+#include <kclangc.h>
+
+#include <unistd.h>
+
+// Note: see config.h for most nxweb #defined parameters
+
+#define NXWEB_LISTEN_HOST_AND_PORT ":8055"
+#define NXWEB_LISTEN_HOST_AND_PORT_SSL ":8056"
+
+#define NXWEB_DEFAULT_CHARSET "utf-8"
+#define NXWEB_DEFAULT_INDEX_FILE "index.htm"
+
+// All paths are relative to working directory:
+#define SSL_CERT_FILE "ssl/server_cert.pem"
+#define SSL_KEY_FILE "ssl/server_key.pem"
+#define SSL_DH_PARAMS_FILE "ssl/dh.pem"
+
+#define SSL_PRIORITIES "NORMAL:+VERS-TLS-ALL:+COMP-ALL:-CURVE-ALL:+CURVE-SECP256R1"
+
+// Setup modules & handlers (this can be done from any file linked to nxweb):
+
+extern nxweb_handler hello_handler;
+extern nxweb_handler benchmark_handler;
+extern nxweb_handler benchmark_handler_inworker;
+extern nxweb_handler test_handler;
+extern nxweb_handler sendfile_handler;
+
+extern nxweb_handler upload_file_handler;
+extern nxweb_handler download_handler;
+extern nxweb_handler fetch_handler;
+extern nxweb_handler delete_handler;
+extern nxweb_handler delete_batch_handler;
+extern nxweb_handler clean_handler;
+extern nxweb_handler status_handler;
+extern nxweb_handler list_handler;
+
+
+KCDB* g_kcdb;
+//KCCUR * g_kccur;
+int RECORD_HEADER_LEN = 4;
+int MAX_UPLOAD_SIZE = 80; 
+
+// These are benchmarking handlers (see modules/benchmark.c):
+NXWEB_SET_HANDLER(benchmark, "/benchmark-inprocess", &benchmark_handler, .priority=100);
+NXWEB_SET_HANDLER(benchmark_inworker, "/benchmark-inworker", &benchmark_handler_inworker, .priority=100);
+NXWEB_SET_HANDLER(test, "/test", &test_handler, .priority=900);
+
+// This is sample handler (see modules/hello.c):
+NXWEB_SET_HANDLER(hello, "/hello", &hello_handler, .priority=1000, .filters={
+#ifdef WITH_ZLIB
+        &gzip_filter
+#endif
+        });
+
+// This is sample handler (see modules/upload.c):
+NXWEB_SET_HANDLER(upload, "/upload_file", &upload_file_handler, .priority=1000);
+//NXWEB_SET_HANDLER(upload_keyvalue, "/upload_data", &upload_file_handler, .priority=1000);
+NXWEB_SET_HANDLER(download, "/download", &download_handler, .priority=1000);
+NXWEB_SET_HANDLER(fetch, "/fetch", &fetch_handler, .priority=1000);
+NXWEB_SET_HANDLER(delete, "/delete", &delete_handler, .priority=1000);
+NXWEB_SET_HANDLER(deletefiles, "/deletefiles", &delete_batch_handler, .priority=1000);
+NXWEB_SET_HANDLER(clean, "/clean", &clean_handler, .priority=1000);
+NXWEB_SET_HANDLER(status, "/status", &status_handler, .priority=1000);
+NXWEB_SET_HANDLER(list, "/list", &list_handler, .priority=1000);
+
+// This proxies requests to backend with index 0 (see proxy setup further below):
+NXWEB_SET_HANDLER(backend1, "/backend1", &nxweb_http_proxy_handler, .priority=10000, .idx=0, .uri="",
+        .filters={ &file_cache_filter, &templates_filter, &ssi_filter },
+        .file_cache_dir="www/cache/proxy");
+
+// This proxies requests to backend with index 1 (see proxy setup further below):
+NXWEB_SET_HANDLER(backend2, "/backend2", &nxweb_http_proxy_handler, .priority=10000, .idx=1, .uri="",
+        .filters={ &file_cache_filter, &templates_filter, &ssi_filter },
+        .file_cache_dir="www/cache/proxy");
+
+// This serves static files from $(work_dir)/www/root directory:
+NXWEB_SET_HANDLER(sendfile, 0, &sendfile_handler, .priority=900000,
+        .filters={
+        &templates_filter,
+        &ssi_filter,
+#ifdef WITH_IMAGEMAGICK
+        &image_filter,
+#endif
+#ifdef WITH_ZLIB
+        &gzip_filter
+#endif
+        }, .dir="www/root",
+        .charset=NXWEB_DEFAULT_CHARSET, .index_file=NXWEB_DEFAULT_INDEX_FILE,
+        .gzip_dir="www/cache/gzip", .img_dir="www/cache/img", .cache=1);
+
+
+// Command-line options:
+static const char* user_name=0;
+static const char* group_name=0;
+static int port=8055;
+static int ssl_port=8056;
+
+
+// Server main():
+
+static void server_main() {
+
+    // Bind listening interfaces:
+    char host_and_port[32];
+    snprintf(host_and_port, sizeof(host_and_port), ":%d", port);
+    if (nxweb_listen(host_and_port, 4096)) return; // simulate normal exit so nxweb is not respawned
+#ifdef WITH_SSL
+    char ssl_host_and_port[32];
+    snprintf(ssl_host_and_port, sizeof(ssl_host_and_port), ":%d", ssl_port);
+    if (nxweb_listen_ssl(ssl_host_and_port, 1024, 1, SSL_CERT_FILE, SSL_KEY_FILE, SSL_DH_PARAMS_FILE, SSL_PRIORITIES)) return; // simulate normal exit so nxweb is not respawned
+#endif // WITH_SSL
+
+    // Drop privileges:
+    if (nxweb_drop_privileges(group_name, user_name)==-1) return;
+
+    // Override default timers (if needed):
+    // make sure the timeout less than 5 seconds. be sure to void receive SIGALRM to shutdown the db brute.
+    //
+    // alarm is set to be 60000000
+    nxweb_set_timeout(NXWEB_TIMER_KEEP_ALIVE, 30000000);
+
+
+    // Go!
+    nxweb_run();
+
+    //if (g_kccur);
+    //    kccurdel(g_kccur);
+
+    if( g_kcdb)
+    {
+        kcdbclose( g_kcdb );
+        g_kcdb = NULL;
+        printf("db closed.\n");
+    }
+}
+
+
+// Utility stuff:
+
+static void show_help(void) {
+    printf( "usage:    adfslite <options>\n\n"
+            " -d       run as daemon\n"
+            " -s       shutdown nxweb\n"
+            " -m mem   set memory map size in MB (default: 64)\n"
+            " -M fMax  set file max size in MB   (default: 80)\n"
+            " -w dir   set work dir    (default: ./)\n"
+            " -l file  set log file    (default: stderr or nxweb_error_log for daemon)\n"
+            " -p file  set pid file    (default: nxweb.pid)\n"
+            " -u user  set process uid\n"
+            " -g group set process gid\n"
+            " -P port  set http port\n"
+            " -x path  database file   (default: ./store.kch)\n"
+#ifdef WITH_SSL
+            " -S port  set https port\n"
+#endif
+            " -h       show this help\n"
+            " -v       show version\n"
+            "\n"
+            "example:  nxweb -d -l nxweb_error_log\n\n"
+          );
+}
+
+int main(int argc, char** argv) 
+{
+    int daemon=0;
+    int shutdown=0;
+    const char* work_dir=0;
+    const char* log_file=0;
+    const char* pid_file="adfslite.pid";
+    const char* dbpath="./store.kch";
+
+    size_t mem_size = 64;
+
+    int c;
+    while ((c=getopt(argc, argv, ":hvdsm:M:w:l:p:u:g:P:x:S:"))!=-1) {
+        switch (c) {
+            case 'h':
+                show_help();
+                return 0;
+            case 'v':
+                printf( "VERSION:      adfslite - 3.2.2\n"
+                        "BUILD-DATE:   "__DATE__ " " __TIME__"\n" );
+                return 0;
+            case 'd':
+                daemon=1;
+                break;
+            case 's':
+                shutdown=1;
+                break;
+            case 'm':
+                mem_size = atoi(optarg);
+                break;
+            case 'M':
+                MAX_UPLOAD_SIZE = atoi(optarg);
+                break;
+            case 'w':
+                work_dir=optarg;
+                break;
+            case 'l':
+                log_file=optarg;
+                break;
+            case 'p':
+                pid_file=optarg;
+                break;
+            case 'u':
+                user_name=optarg;
+                break;
+            case 'g':
+                group_name=optarg;
+                break;
+            case 'x':
+                dbpath = optarg;
+                break;
+            case 'P':
+                port=atoi(optarg);
+                if (port<=0) {
+                    fprintf(stderr, "invalid port: %s\n\n", optarg);
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'S':
+                ssl_port=atoi(optarg);
+                if (ssl_port<=0) {
+                    fprintf(stderr, "invalid ssl port: %s\n\n", optarg);
+                    return EXIT_FAILURE;
+                }
+                break;
+            case '?':
+                fprintf(stderr, "unkown option: -%c\n\n", optopt);
+                show_help();
+                return EXIT_FAILURE;
+        }
+    }
+
+    if ((argc-optind)>0) {
+        fprintf(stderr, "too many arguments\n\n");
+        show_help();
+        return EXIT_FAILURE;
+    }
+
+    if (shutdown) {
+        nxweb_shutdown_daemon(work_dir, pid_file);
+        return EXIT_SUCCESS;
+    }
+
+    // using kchashdb to store files.
+    g_kcdb = kcdbnew();
+    char path[1024] = {0};
+    mem_size *= 1024*1024;
+    MAX_UPLOAD_SIZE *= 1024*1024;
+    sprintf(path, "%s#apow=%d#fpow=%d#msiz=%lu#dfunit=%d",
+            dbpath, 10, 10, mem_size, 8);
+
+    int32_t succ = kcdbopen( g_kcdb, path, KCOWRITER|KCOCREATE);
+    if( succ )
+        printf("using dbpath :%s\n", dbpath );
+    else{
+        printf("db open failed:%s\n", dbpath );
+        exit(-1);
+    }
+
+    //g_kccur = kcdbcursor(g_kcdb);
+
+    if (daemon) {
+        if (!log_file) log_file="nxweb_error_log";
+        nxweb_run_daemon(work_dir, log_file, pid_file, server_main);
+    }
+    else {
+        nxweb_run_normal(work_dir, log_file, pid_file, server_main);
+
+    }
+    return EXIT_SUCCESS;
+}
+
---
+++ src/bin/modules/download.c
@@ -0,0 +1,441 @@
+
+#include "nxweb/nxweb.h"
+#include <kclangc.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+extern KCDB* g_kcdb;
+//extern KCCUR* g_kccur;
+extern int RECORD_HEADER_LEN;
+
+
+static const char upload_handler_key;
+#define UPLOAD_HANDLER_KEY ((nxe_data)&upload_handler_key)
+
+#define MAX_COUNT_PER_LIST 10000
+
+
+typedef struct KC_DATA{
+    char * data_ptr;
+}KC_DATA;
+
+
+static void upload_request_data_finalize(
+        nxweb_http_server_connection *conn,
+        nxweb_http_request *req,
+        nxweb_http_response *resp,
+        nxe_data data)
+{
+    KC_DATA * d = data.ptr;
+    if( d->data_ptr ){
+        kcfree( d->data_ptr );
+        d->data_ptr = NULL;
+    }
+}
+
+
+static nxweb_result download_on_request(nxweb_http_server_connection* conn, nxweb_http_request* req, nxweb_http_response* resp) 
+{
+    printf("download - request\n");
+    
+    // get the namespace. defaults using 
+    nxweb_parse_request_parameters( req, 0 );
+    const char *name_space = nx_simple_map_get_nocase( req->parameters, "namespace" );
+
+    char * url = malloc(strlen(req->path_info)+1);
+    if (!url) {return NXWEB_ERROR;}
+    strcpy(url, req->path_info);
+    url[strlen(req->path_info)] = '\0';
+
+    nxweb_url_decode( url, 0 );
+    char *fname = url, *temp;
+    while( temp = strstr( fname, "/" ) )
+        fname = temp + 1;
+
+    char *strip_args = strstr( fname, "?" );
+
+    int fname_len = strip_args ? (strip_args - fname) : strlen(fname);
+    char fname_with_space[1024] = {0};
+    if ( strlen(url) > sizeof(fname_with_space)-1 ) {
+        char err_msg[1024] = {0};
+        snprintf( err_msg, sizeof(err_msg)-1, 
+                "<html><body>File not found<br/>namespace:%s<br/>filename:%.*s</body></html>", 
+                name_space, fname_len, fname );
+        nxweb_send_http_error( resp, 404, err_msg );
+	resp->keep_alive=0;
+        free(url);
+        return NXWEB_MISS;
+    }
+
+    if( name_space ) {sprintf( fname_with_space, "%s:/", name_space);}
+    else {strcpy( fname_with_space, ":/" );}
+
+    strncat( fname_with_space, fname, fname_len );
+
+    size_t file_size = 0;
+    char *pfile_data = kcdbget( g_kcdb, fname_with_space, strlen(fname_with_space), &file_size );
+    if( pfile_data == NULL )
+    {
+        char err_msg[1024] = {0};
+        snprintf( err_msg, sizeof(err_msg)-1, 
+                "<html><body>File not found<br/>namespace:%s<br/>filename:%.*s</body></html>", 
+                name_space, fname_len, fname );
+        nxweb_send_http_error( resp, 404, err_msg );
+	resp->keep_alive=0;
+        free(url);
+        return NXWEB_MISS;
+    }
+
+    KC_DATA *ptmp = nxb_alloc_obj(req->nxb, sizeof(KC_DATA));
+    nxweb_set_request_data(req, UPLOAD_HANDLER_KEY, (nxe_data)(void *)ptmp, upload_request_data_finalize);
+    ptmp->data_ptr = pfile_data;
+
+    nxweb_send_data( resp, pfile_data + RECORD_HEADER_LEN, file_size - RECORD_HEADER_LEN, "application/octet-stream" );
+
+    free(url);
+    return NXWEB_OK;
+}
+
+
+static void fetch_request_data_finalize(
+        nxweb_http_server_connection *conn,
+        nxweb_http_request *req,
+        nxweb_http_response *resp,
+        nxe_data data)
+{
+    KC_DATA * d = data.ptr;
+    if( d->data_ptr ){
+        kcfree( d->data_ptr );
+        d->data_ptr = NULL;
+    }
+}
+
+static nxweb_result fetch_on_request(
+        nxweb_http_server_connection* conn, 
+        nxweb_http_request* req, 
+        nxweb_http_response* resp) 
+{
+    printf("fetch - request\n");
+
+    // get the namespace. defaults using 
+    nxweb_parse_request_parameters( req, 0 );
+    const char *name_space = nx_simple_map_get_nocase( req->parameters, "namespace" );
+
+    char * url = malloc(strlen(req->path_info)+1);
+    if (!url) 
+        return NXWEB_ERROR;
+    strcpy(url, req->path_info);
+    url[strlen(req->path_info)] = '\0';
+
+    nxweb_url_decode( url, 0 );
+    char *fname = url, *temp;
+
+    while( temp = strstr( fname, "/" ) )
+        fname = temp + 1;
+
+    char *strip_args = strstr( fname, "?" );
+    int fname_len = strip_args ? (strip_args-fname) : strlen( fname );
+    char fname_with_space[1024]= {0};
+    if( strlen( url ) > sizeof( fname_with_space )-1 )
+    {
+        char err_msg[1024] = {0};
+        snprintf( err_msg, sizeof(err_msg)-1, 
+                "File not found<br/>namespace:%s<br/>filename:%.*s" , name_space, fname_len, fname);
+        nxweb_send_http_error( resp, 404, err_msg );
+	resp->keep_alive=0;
+        free(url);
+        return NXWEB_MISS;
+    }
+
+    if( name_space ) {sprintf( fname_with_space, "%s:/", name_space);}
+    else {strcpy( fname_with_space, ":/" );}
+    strncat( fname_with_space, fname, fname_len );
+
+    size_t file_size = 0;
+    char *pfile_data = kcdbseize( g_kcdb, fname_with_space, strlen( fname_with_space), &file_size );
+    if( pfile_data == NULL )
+    {
+        char err_msg[1024] = {0};
+        snprintf( err_msg, sizeof(err_msg)-1, 
+                "File not found<br/>namespace:%s<br/>filename:%.*s", name_space, fname_len, fname);
+        nxweb_send_http_error( resp, 404, err_msg );
+	resp->keep_alive=0;
+        free(url);
+        return NXWEB_MISS;
+    }
+
+    KC_DATA *ptmp = nxb_alloc_obj(req->nxb, sizeof(KC_DATA));
+    nxweb_set_request_data(req, UPLOAD_HANDLER_KEY, (nxe_data)(void *)ptmp, fetch_request_data_finalize);
+    ptmp->data_ptr = pfile_data;
+
+    nxweb_send_data( resp, pfile_data + RECORD_HEADER_LEN, file_size - RECORD_HEADER_LEN, "application/octet-stream" );    
+    free(url);
+    return NXWEB_OK;
+}
+
+
+static nxweb_result delete_on_request(
+        nxweb_http_server_connection* conn, 
+        nxweb_http_request* req, 
+        nxweb_http_response* resp) 
+{
+    printf("delete - request\n");
+
+    nxweb_set_response_content_type(resp, "text/plain");
+
+    // get the namespace. defaults using 
+    nxweb_parse_request_parameters( req, 0 );
+    const char *name_space = nx_simple_map_get_nocase( req->parameters, "namespace" );
+
+    char * url = malloc(strlen(req->path_info)+1);
+    if (!url) 
+    {
+        nxweb_send_http_error( resp, 500, "Please retry" );
+	resp->keep_alive=0;
+        return NXWEB_ERROR;
+    }
+    strcpy(url, req->path_info);
+    url[strlen(req->path_info)] = '\0';
+
+    nxweb_url_decode( url, 0 );
+    char *fname = url, *temp;
+
+    while( temp = strstr( fname, "/" ) ) {fname = temp + 1;}
+
+    char *strip_args = strstr( fname, "?" );
+    int fname_len = strip_args?strip_args - fname:strlen( fname );
+    char fname_with_space[1024]= {0};
+    if( strlen( url ) > sizeof( fname_with_space )-1 ) {
+        nxweb_send_http_error( resp, 414, "Request URI Too Long");
+	resp->keep_alive=0;
+        free(url);
+        return NXWEB_MISS;
+    }
+
+    if( name_space )
+        sprintf( fname_with_space, "%s:/", name_space);
+    else
+        strcpy( fname_with_space, ":/" );
+    strncat( fname_with_space, fname, fname_len );
+
+    kcdbremove( g_kcdb, fname_with_space, strlen( fname_with_space) );
+    nxweb_response_printf( resp, "OK\n" );
+    free(url);
+    return NXWEB_OK;
+}
+
+static nxweb_result delete_batch_on_request(
+        nxweb_http_server_connection* conn, 
+        nxweb_http_request* req, 
+        nxweb_http_response* resp) 
+{
+    printf("delete-batch - request\n");
+
+    nxweb_set_response_content_type(resp, "text/plain");
+
+    // get the namespace. defaults using 
+    nxweb_parse_request_parameters( req, 0 );
+    const char *name_space = nx_simple_map_get_nocase( req->parameters, "namespace" );
+    const char *fname_raw = nx_simple_map_get_nocase( req->parameters, "names" );
+
+    if (!fname_raw) {
+        nxweb_response_printf(resp, "url error\nexample: http://127.0.0.1:8055/deletefiles?names=f1[:f2][&namespace=np]\n");
+        return NXWEB_ERROR;
+    }
+
+    KCSTR  kc_fnames[400] = {0};
+    char fnames_with_space[400][1024] = {0};
+    char *pC = (char *)fname_raw;
+    int fname_cnt = 0;
+    int fname_len = 0;
+    do{
+        char *pT = strstr( pC, ":" );
+        if( pT ){
+            fname_len = pT - pC;
+            pT++;
+        }
+        else{
+            fname_len = strlen( pC );
+        }
+        if( fname_len == 0 ){
+            pC = pT;
+            continue;
+        }
+        //printf("CurrPC:%s\n", pC );
+        if( name_space )
+            strcpy( fnames_with_space[fname_cnt], name_space );
+        strcat( fnames_with_space[fname_cnt], ":/" );
+        strncat( fnames_with_space[fname_cnt], pC, fname_len );
+        kc_fnames[fname_cnt].buf = fnames_with_space + fname_cnt;
+        kc_fnames[fname_cnt].size = strlen( fnames_with_space[fname_cnt] );
+        pC = pT;
+        fname_cnt++;
+    } while(pC);
+
+    int i=0;
+    for(; i<fname_cnt; ++i)
+    {
+        char pBuf[1024] = {0};
+        memcpy( pBuf,  kc_fnames[i].buf, kc_fnames[i].size );
+    }
+
+    kcdbremovebulk( g_kcdb, kc_fnames, fname_cnt, 0 );
+    nxweb_response_printf( resp, "OK\n" );
+}
+
+// called by clean function /////////////////////////
+void WipeData(int interval_hour)
+{
+    // traverse db
+    // read file and get time
+    // if time stamp earlier than 'time_line'
+
+    int time_now = (int)time(NULL);
+    int time_line = time_now - interval_hour * 60 * 60;
+    int temp = 0;
+    char *kbuf = 0;
+    const char* cvbuf = 0;
+    size_t ksize, vsize;
+
+    KCCUR * cur = kcdbcursor(g_kcdb);
+    kccurjump(cur);
+
+    while ( ( kbuf = kccurget(cur, &ksize, &cvbuf, &vsize, 0)) != NULL )
+    {
+        memcpy(&temp, cvbuf, RECORD_HEADER_LEN);
+        //printf("%d, k:%d, ks:%d, v:%d, vs:%d\n", temp, kbuf, ksize, cvbuf, vsize);
+        if (temp < time_line)
+            kccurremove(cur);
+        else
+            kccurstep(cur);
+    }
+
+    kccurdel(cur);
+
+    return ;
+}
+
+
+/////////////// clean //////////////////////
+static nxweb_result clean_on_request(
+        nxweb_http_server_connection* conn, 
+        nxweb_http_request* req, 
+        nxweb_http_response* resp) 
+{
+    printf("clean_on_request\n");
+
+    if (strlen(req->path_info) <= 1 || strlen(req->path_info) >= 4 ) {
+        nxweb_send_http_error(resp, 403, "NUMBER error!\nExample:\nhttp://host:port/clean/24");
+	resp->keep_alive=0;
+        return NXWEB_ERROR;
+    }
+
+    char tmp[8] = {0};
+    strcpy(tmp, (req->path_info) +1);
+    int interval_hour = atoi(tmp);
+    if (interval_hour < 0 || interval_hour > 99)
+    {
+        nxweb_send_http_error(resp, 403, "NUMBER error!\nExample:\nhttp://host:port/clean/24");
+	resp->keep_alive=0;
+        return NXWEB_ERROR;
+    }
+
+    WipeData(interval_hour);
+    nxweb_response_printf(resp, "OK");
+    return NXWEB_OK;
+}
+
+
+/////////////// status /////////////////////
+static nxweb_result status_on_request(nxweb_http_server_connection* conn, nxweb_http_request* req, nxweb_http_response* resp) 
+{
+    printf("status - request\n");
+    nxweb_set_response_content_type(resp, "text/plain");
+    char *status = kcdbstatus( g_kcdb );
+
+    size_t len = 2*strlen(status);
+    char *buf = malloc(len);
+    memset(buf, 0, len);
+
+    int i=0, j=0;
+    for (i=0; i<strlen(status); i++) {
+        if (status[i] == '\t') {
+            strcat(buf, ": ");
+            j += 2;
+        }
+        else {
+            buf[j] = status[i];
+            j++;
+        }
+    }
+
+    nxweb_response_printf( resp, "%s\n", buf );
+    kcfree( status );
+    status = NULL;
+    free(buf);
+    return NXWEB_OK;
+}
+
+
+/////////////// list all names /////////////////////
+static nxweb_result list_on_request(nxweb_http_server_connection* conn, nxweb_http_request* req, nxweb_http_response* resp) 
+{
+    printf("list - request\n");
+
+    nxweb_set_response_content_type(resp, "text/plain");
+    const char *name_space = nx_simple_map_get_nocase( req->parameters, "namespace" );
+    const char *prefix = nx_simple_map_get_nocase( req->parameters, "prefix" );
+    char kc_prefix[1024] = {0};
+    if( name_space ){
+        strncpy( kc_prefix, name_space, sizeof( kc_prefix )-3 );
+        strcat( kc_prefix, ":/" );
+        if( prefix ){
+            strncat( kc_prefix, prefix, sizeof(kc_prefix) - strlen( kc_prefix ) - 1 );
+        }
+    }
+    printf("list prefix:%s\n", kc_prefix );
+    char *fnames[MAX_COUNT_PER_LIST] = {0};
+    int cnt = kcdbmatchprefix (g_kcdb, kc_prefix, fnames, MAX_COUNT_PER_LIST );
+    int i = 0;
+
+    while( i< cnt ){
+        nxweb_response_printf( resp, "%s\n", fnames[i] );
+        kcfree( fnames[i] );
+        fnames[i] = NULL;
+        i++;
+    }
+    return NXWEB_OK;
+}
+
+
+nxweb_handler download_handler={
+    .on_request = download_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
+nxweb_handler fetch_handler={
+    .on_request = fetch_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
+nxweb_handler delete_handler={
+    .on_request = delete_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
+nxweb_handler delete_batch_handler={
+    .on_request = delete_batch_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
+nxweb_handler clean_handler={
+    .on_request = clean_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
+nxweb_handler status_handler={
+    .on_request = status_on_request};
+
+nxweb_handler list_handler={
+    .on_request = list_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS};
+
